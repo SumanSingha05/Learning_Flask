87d18db3fa52cc5f3e7d0583c24c9f620a09d40d
SumanSingha05 <sumansingha8654@gmail.com>
2025-12-23 17:48:07 +0530

    Remove login functionality, add home and about pages with templates.

--- app.py
+++ app.py
@@ -3,24 +3,9 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def login():
-    return render_template("login.html")
+def home():
+    return render_template("home.html")
 
-@app.route("/submit", methods = ["POST"])
-def submit():
-    username= request.form.get("username")
-    password = request.form.get("password")
-
-    # if username == "suman" and password == "123":
-    #     return render_template("welcome.html", name = username)
-    
-    valid_users = {
-        'admin':'123',
-        'suman':'1234',
-    }
-
-    if username in valid_users and password == valid_users[username]:
-        return render_template("welcome.html", name = username)
-    
-    else:
-        return "Invalid Credentials"
+@app.route("/about")
+def about():
+    return render_template("about.html")
