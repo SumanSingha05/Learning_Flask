45ce3628fcff715f7ce280a064507e0a1413183b
SumanSingha05 <sumansingha8654@gmail.com>
2025-12-24 13:27:59 +0530

    Manual form handling, request, redirect, CRSF token.

---
+++ forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+class RegistrationForm(FlaskForm):
+    name = StringField("Full Name", validators=[DataRequired(message= "we need your name, it can't be empty")])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=6)])
+    submit = SubmitField("Register")
